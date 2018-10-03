ec3b49acd558d0c4e48377f1652d83a2652c2121
qiankaijie <qiankaijie1024@gmail.com>
2018-10-03 10:49:18 +0800

    折线图

--- src/pages/equipment/equipment.ts
+++ src/pages/equipment/equipment.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import * as echarts from 'echarts'; 
 
 /**
  * Generated class for the EquipmentPage page.
@@ -18,8 +19,25 @@ export class EquipmentPage {
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad EquipmentPage');
-  }
+ ionViewDidLoad() {
+    console.log('ionViewDidLoad DayWeatherPage');
+    const ec = echarts as any;
+        var myChart = ec.init(document.getElementById('chart'));
+        var optionchart = {
+            xAxis: {
+                type: 'category',
+                data: ['1', '6', '12', '18', '24']
+            },
+            yAxis: {
+                type: 'value'
+            },
+            series: [{
+                data: [820, 932, 901, 934, 1290],
+                type: 'line'
+            }]
+        };
 
+        myChart.setOption(optionchart);
+
+  }
 }
