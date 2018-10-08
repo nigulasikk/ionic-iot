bb85a51f7111427a6273f1faddb74f0921c440e3
qiankaijie <qiankaijie1024@gmail.com>
2018-10-08 22:55:12 +0800

    action style

--- src/pages/action/action.module.ts
+++ src/pages/action/action.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { ActionPage } from './action';
+import { ComponentsModule } from './../../components/components.module';
 
 @NgModule({
   declarations: [
     ActionPage,
   ],
   imports: [
-    IonicPageModule.forChild(ActionPage),
+    IonicPageModule.forChild(ActionPage),ComponentsModule,
   ],
 })
 export class ActionPageModule {}
--- src/pages/action/action.ts
+++ src/pages/action/action.ts
@@ -14,12 +14,17 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'action.html',
 })
 export class ActionPage {
+  city : string= '1';
+  filter : string= 'ps';
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ActionPage');
+  }
+  getItems(e){
+    console.log(e);
   }
 
 }
