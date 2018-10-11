59abc6dc87e01d3e4f2094f4da4246caacc15a8b
qiankaijie <qiankaijie1024@gmail.com>
2018-10-11 21:28:39 +0800

    login & tutorial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,22 +8,7 @@ import { FirstRunPage } from '../pages';
 import { Settings } from '../providers';
 
 @Component({
-  template: `<ion-menu [content]="content">
-    <ion-header>
-      <ion-toolbar>
-        <ion-title>Pages</ion-title>
-      </ion-toolbar>
-    </ion-header>
-
-    <ion-content>
-      <ion-list>
-        <button menuClose ion-item *ngFor="let p of pages" (click)="openPage(p)">
-          {{p.title}}
-        </button>
-      </ion-list>
-    </ion-content>
-
-  </ion-menu>
+  template: `
   <ion-nav #content [root]="rootPage"></ion-nav>`
 })
 export class MyApp {
@@ -32,22 +17,7 @@ export class MyApp {
   @ViewChild(Nav) nav: Nav;
 
   pages: any[] = [
-    { title: 'EquipmentPhoto', component: 'EquipmentPhotoPage' },
-    { title: 'Equipment', component: 'EquipmentPage' },
-    { title: 'DayClimate', component: 'DayClimatePage' },
-    { title: 'DayWeather', component: 'DayWeatherPage' },
-    { title: 'Weather', component: 'WeatherPage' },
-    { title: 'Tutorial', component: 'TutorialPage' },
-    { title: 'Welcome', component: 'WelcomePage' },
-    { title: 'Tabs', component: 'TabsPage' },
-    { title: 'Cards', component: 'CardsPage' },
-    { title: 'Content', component: 'ContentPage' },
-    { title: 'Login', component: 'LoginPage' },
-    { title: 'Signup', component: 'SignupPage' },
-    { title: 'Master Detail', component: 'ListMasterPage' },
-    { title: 'Menu', component: 'MenuPage' },
-    { title: 'Settings', component: 'SettingsPage' },
-    { title: 'Search', component: 'SearchPage' }
+   
   ]
 
   constructor(private translate: TranslateService, platform: Platform, settings: Settings, private config: Config, private statusBar: StatusBar, private splashScreen: SplashScreen) {
---
+++ src/pages/login/login.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { IonicPage, NavController, ToastController } from 'ionic-angular';
+
+import { User } from '../../providers';
+
+@IonicPage()
+@Component({
+  selector: 'page-login',
+  templateUrl: 'login.html'
+})
+export class LoginPage {
+  // The account fields for the login form.
+  // If you're using the username field with or without email, make
+  // sure to add it to the type
+  account: { email: string, password: string } = {
+    email: 'test@example.com',
+    password: 'test'
+  };
+
+  constructor(public navCtrl: NavController,
+    public user: User,
+    public toastCtrl: ToastController,
+    public translateService: TranslateService) {
+
+    this.translateService.get('LOGIN_ERROR').subscribe((value) => {
+      this.loginErrorString = value;
+    })
+  }
+
+  // Attempt to login in through our User service
+  doLogin() {
+    this.navCtrl.setRoot('WeatherPage', {}, {
+      animate: true,
+      direction: 'forward'
+    });
+  }
+}
